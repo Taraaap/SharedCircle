0fa5e26de389c958d2d7f5a7ad68e0a7f0786cd1
tarapaudel542@gmail.com <tarapaudel542@gmail.com>
2026-08-10 11:52:59 +0545

    friends page

---
+++ Controllers/FriendsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SharedCircle.Data;
+using SharedCircle.Models;
+
+namespace SharedCircle.Controllers
+{
+    [Authorize]
+    public class FriendsController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public FriendsController(
+            ApplicationDbContext db,
+            UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            if (currentUser == null)
+                return Unauthorized();
+
+            var friends = await _db.Follows
+                .Where(f =>
+                    f.FollowerId == currentUser.Id &&
+                    _db.Follows.Any(reverse =>
+                        reverse.FollowerId == f.FollowingId &&
+                        reverse.FollowingId == currentUser.Id))
+                .Include(f => f.Following)
+                .Select(f => f.Following)
+                .ToListAsync();
+
+            return View(friends);
+        }
+    }
+}
